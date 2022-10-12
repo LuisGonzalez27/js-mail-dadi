8515d929b63dbab51cc366536760d0bf77aefa33
LuisGonzalez27 <luis27coding@gmail.com>
2022-10-12 16:45:48 +0200

    Gioco dei dadi

--- js/script.js
+++ js/script.js
@@ -32,3 +32,31 @@ function(){
     document.getElementById('userEmail').value = " ";
 }
 )
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+const lancia = document.getElementById('btn_lancia');
+
+lancia.addEventListener('click',
+function(){
+    // Genera un numero random per l'utente
+    let numUser = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("user").innerHTML = "Il numero generato per l'utente è: " + numUser;
+
+    //Genera un numero random per il computer
+    let numComputer = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("computer").innerHTML = "Il numero generato per il computer è: " + numComputer;
+
+    // Confronta i due numeri e stabilisci il punteggio più alto quindi il vincitore
+    if (numUser > numComputer) {
+        console.log("L'utente vince");
+        document.getElementById("winner").innerHTML = "Il vincitore è l'utente!";
+    } else if (numUser < numComputer) {
+        console.log("il computer vince");
+        document.getElementById("winner").innerHTML = "Il vincitore è il computer!";
+    } else {
+        console.log("Pareggio");
+        document.getElementById("winner").innerHTML = "Pareggio!";
+    }
+}
+)
